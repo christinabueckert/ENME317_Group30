6e7e51e7124be0efdc17d38963a88518319be070
Tim28rc <tim.chang.4916@gmail.com>
2018-11-20 15:17:41 -0700

    Line format

--- ENME337_Group30_FinalProject.m
+++ ENME337_Group30_FinalProject.m
@@ -1,6 +1,7 @@
+%{
 % ENME 337, Final Project 
 % Due Dec 3, 2018 WOOOOOO WOOOOO #2
-
+%}
 close all; clear; clc;
 
 p = 1.23;
@@ -8,15 +9,9 @@ a_c = 0.2;
 cut_in = 3;
 cut_off = 25; 
 
-load('radius.dat');
-load('omega.dat');
-load('twist.dat');
-load('chord.dat');
-load('DU21.dat');
-load('DU30.dat');
-load('DU35.dat');
-load('DU40.dat');
-load('NACA64.dat');
+load('radius.dat'); load('omega.dat'); load('twist.dat');
+load('chord.dat'); load('DU21.dat'); load('DU30.dat');
+load('DU35.dat'); load('DU40.dat'); load('NACA64.dat');
 
 w = omega';     % rotational speed for different wind speeds
 r = radius';    % radial positions along the blade r 
