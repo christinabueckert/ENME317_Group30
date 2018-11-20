3b0ce0fdad1efafedbdd759413a17cba28e3ee6d
Bradley Dezall <bradleydezall@gmail.com>
2018-11-20 14:48:19 -0700

    Add files

--- ENME337_Group30_FinalProject.m
+++ ENME337_Group30_FinalProject.m
@@ -1,3 +1,19 @@
 % ENME 337, Final Project 
 % Due Dec 3, 2018 WOOOOOO WOOOOO #2
-% Test
+
+close all; clear; clc;
+
+p = 1.23;
+a_c = 0.2; 
+cut_in = 3;
+cut_off = 25; 
+
+load('radius.dat');
+load('omega.dat');
+load('twist.dat');
+load('chord.dat');
+load('DU21.dat');
+load('DU30.dat');
+load('DU35.dat');
+load('DU40.dat');
+load('NACA64.dat');
