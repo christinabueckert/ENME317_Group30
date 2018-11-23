20d0364f2acd401b11f4ae5c5ba3f4d39fafe78c
jordodanewf <45154737+jordodanewf@users.noreply.github.com>
2018-11-23 11:18:39 -0700

    Delete PwrLaw.m

--- PwrLaw.m
+++
@@ -1,7 +0,0 @@
-function [v0] = PwrLaw(h,v)
-%This function uses the power law to calculate the wind speed at the
-%hub height with inputs of wind speed at some given height
-H=15; %all of our windspeed data is at 15m
-v0=round(v*(h/H)^(1/7)); % rounds the power law to nearest integer
-end
-
