7a31429acb8476836602e42e82c32afb97dcd80f
Bradley Dezall <bradleydezall@gmail.com>
2018-11-29 15:21:26 -0700

    More changes

--- ENME337_Group30_FinalProject.m
+++ ENME337_Group30_FinalProject.m
@@ -39,44 +39,17 @@ load('DataFiles/radius.dat'); load('DataFiles/omega.dat'); load('DataFiles/twist
 load('DataFiles/chord.dat'); load('DataFiles/DU21.dat'); load('DataFiles/DU30.dat');
 load('DataFiles/DU35.dat'); load('DataFiles/DU40.dat'); load('DataFiles/NACA64.dat');
 %Sets of functions wrt r
-r = radius'   ; % radial positions along the blade r 
-chord = chord'; % chord of the blade at different radial positions
-twist = twist'; % twist angle of the blade in degrees at different radial positions 
+r = radius   ; % radial positions along the blade r 
+c = chord; % chord of the blade at different radial positions
+twist = (pi/180).*twist; % twist angle of the blade in degrees at different radial positions 
 
-w = omega';    % rotational speed for different wind speeds
+w = (2*pi/60).*omega;    % rotational speed for different wind speeds
 B = 3;
-%just plot this for the hell of it (you can get rid of this later)
-%plot(r,chord,r,twist);
+V0 = 1:1:25;
 
 %% Computation of Power Production
-% (1)  -  Initialize a? and a.
-a = 100;
-a_p = 100;
+Power  = power_calculation(V0,B,w,twist,c,r,p,a_c);
 
-% (2)  -  Compute the flow angle.
-tan_psi = @(A, Ap, V0, Omega, R) ((1-A)*V0)/((1+Ap)*Omega*R); %adds function for tan(psi)
-psi = @(A, Ap, V0, Omega, R) arctan(tan_psi(A, Ap, V0, Omega, R)); %inverse tans to find psi
-
-% (3)  -  Compute local angle of attack
-alpha = @(Psi,Theta) Psi - Theta;
-
-% (4)  -  Determine C1(alpha) and C2(alpha) according to the given airfoil profile
-
-% (5)  -  Compute normalized force Cn and Ct
-Cn = @(Cl,Cd,Psi) (Cl*cos(Psi))+(Cd*sin(Psi));
-Ct = @(Cl,Cd,Psi) (Cl*sin(Psi))-(Cd*cos(Psi));
-
-% (6)  -  Calculate a' and a
-% c = @(R) local chord at position r => I think you need to do the
-% interpolation but who knows
-% sigma = @(R,B)  
-
-
-% (7)  -  Apply the following correction (called Glauert correction) for
-                %large values of a
-                
-% (8)  -  If a? and a has changed more than a certain tolerance (use 1e-6), go to step (2) or
-                %else proceed to the next step
       %%
    %Plots
    %initalizing matricies for windspeeds based on # of days in month and a
